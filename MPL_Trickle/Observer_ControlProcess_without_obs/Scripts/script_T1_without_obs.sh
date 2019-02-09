41683826a7be6270314bdba0018d1224195bec9c
ThangNGUYEN <minhthang20021991@gmail.com>
2019-02-09 21:12:42 +0100

    Commit for Observer and Control Process with and without instruction obs

---
+++ MPL_Trickle/Observer_ControlProcess_without_obs/Scripts/script_T1_without_obs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+shopt -s expand_aliases
+export MARIMBACH=/usr/share/sounds/ubuntu/ringtones/Marimbach.ogg
+alias Marimbach='paplay $MARIMBACH'
+export HARMONICS=/usr/share/sounds/ubuntu/ringtones/Harmonics.ogg
+alias Harmonics='paplay $HARMONICS'
+
+for i in {0..19..1}
+do
+   /home/thang/git/org.eclipse.efm-symbex/org.eclipse.efm.symbex/Release/avm.exe /home/thang/Dropbox/Implementation_Diversity/Diversity_workspace/MPL_Trickle_WP/Observer_ControlProcess_without_obs/workflow_Observer_ControlProcess_without_obs.sew
+   Marimbach
+done
+
+Harmonics
+
