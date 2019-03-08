3d9e8d5674f795b60f0ae4fb51d4a3764b7b5476
ThangNGUYEN <minhthang20021991@gmail.com>
2019-03-08 09:00:39 +0100

    Modification for scenario 'Outdated for topology 1'

---
+++ MPL_Trickle/Outdated_Topology1/Scripts/script_T1_with_L_D.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+shopt -s expand_aliases
+export MARIMBACH=/usr/share/sounds/ubuntu/ringtones/Marimbach.ogg
+alias Marimbach='paplay $MARIMBACH'
+export HARMONICS=/usr/share/sounds/ubuntu/ringtones/Harmonics.ogg
+alias Harmonics='paplay $HARMONICS'
+
+for i in {0..9..1}
+do
+   /home/thang/git/org.eclipse.efm-symbex/org.eclipse.efm.symbex/Release/avm.exe /home/thang/Dropbox/Implementation_Diversity/Diversity_workspace/MPL_Trickle_With_New_Connectors/Configuration_ports_simplified/SEL2/With_L_D_T1/workflow_At_Least_One_Outdated_SEQ2_FOR2.sew >> /home/thang/Dropbox/Implementation_Diversity/Diversity_workspace/MPL_Trickle_With_New_Connectors/Configuration_ports_simplified/SEL2/With_L_D_T1/Scripts/Log_Symbex.txt 
+   Marimbach
+done
+
+Harmonics
+
